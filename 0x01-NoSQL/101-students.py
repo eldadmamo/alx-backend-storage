05b098f8f5b1278bbde2d10bf8fb65645947a3ef
eldadmamo <eldadf456@gmail.com>
2024-08-12 14:03:18 -0700

    0x01-NoSQL

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+'''
+14 module.
+'''
+
+
+def top_students(mongo_collection):
+    '''Prints students collection sorted average score.
+    '''
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
