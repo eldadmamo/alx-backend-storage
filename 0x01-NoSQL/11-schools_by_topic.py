4f1eefdd1d5292f96943df4e27c644378b9f134c
eldadmamo <eldadf456@gmail.com>
2024-08-12 13:53:34 -0700

    0x01-NoSQL

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+Task 11 module.
+'''
+
+
+def schools_by_topic(mongo_collection, topic):
+    '''Returns the list of school specific topic.
+    '''
+    topic_filter = {
+        'topics': {
+            '$elemMatch': {
+                '$eq': topic,
+            },
+        },
+    }
+    return [doc for doc in mongo_collection.find(topic_filter)]
---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+Task 9 module.
+'''
+
+
+def insert_school(mongo_collection, **kwargs):
+    '''Inserts a new document  collection.
+    '''
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
